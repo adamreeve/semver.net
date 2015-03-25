e3c25071a47904ccc97e04099e8795e76dc399e1
Adam Reeve <adreeve@gmail.com>
2015-03-25 21:31:05 +1300

    Add major, minor, patch version parsing with tests

---
+++ SemVer.Tests/ParseMajorMinorPatch.cs
@@ -0,0 +1,75 @@
+﻿using Xunit;
+using System;
+
+namespace SemVer.Tests
+{
+    public class ParseMajorMinorPatch
+    {
+        [Fact]
+        public void ParseMajorVersion ()
+        {
+            var testCases = new Tuple<string, int>[] {
+                Tuple.Create("1.2.3", 1),
+                Tuple.Create(" 1.2.3 ", 1),
+                Tuple.Create(" 2.2.3-4 ", 2),
+                Tuple.Create(" 3.2.3-pre ", 3),
+                Tuple.Create("v5.2.3", 5),
+                Tuple.Create(" v8.2.3 ", 8),
+                Tuple.Create("\t13.2.3", 13),
+                Tuple.Create("=21.2.3", 21),
+                Tuple.Create("v=34.2.3", 34),
+            };
+
+            foreach (var testCase in testCases)
+            {
+                var version = new SemVer.Version(testCase.Item1);
+                Assert.Equal(version.Major, testCase.Item2);
+            }
+        }
+
+        [Fact]
+        public void ParseMinorVersion ()
+        {
+            var testCases = new Tuple<string, int>[] {
+                Tuple.Create("1.2.3", 2),
+                Tuple.Create(" 1.2.3 ", 2),
+                Tuple.Create(" 2.2.3-4 ", 2),
+                Tuple.Create(" 3.2.3-pre ", 2),
+                Tuple.Create("v5.2.3", 2),
+                Tuple.Create(" v8.2.3 ", 2),
+                Tuple.Create("\t13.2.3", 2),
+                Tuple.Create("=21.2.3", 2),
+                Tuple.Create("v=34.2.3", 2),
+            };
+
+            foreach (var testCase in testCases)
+            {
+                var version = new SemVer.Version(testCase.Item1);
+                Assert.Equal(version.Minor, testCase.Item2);
+            }
+        }
+
+        [Fact]
+        public void ParsePatchVersion ()
+        {
+            var testCases = new Tuple<string, int>[] {
+                Tuple.Create("1.2.3", 3),
+                Tuple.Create(" 1.2.3 ", 3),
+                Tuple.Create(" 2.2.3-4 ", 3),
+                Tuple.Create(" 3.2.3-pre ", 3),
+                Tuple.Create("v5.2.3", 3),
+                Tuple.Create(" v8.2.3 ", 3),
+                Tuple.Create("\t13.2.3", 3),
+                Tuple.Create("=21.2.3", 3),
+                Tuple.Create("v=34.2.3", 3),
+            };
+
+            foreach (var testCase in testCases)
+            {
+                var version = new SemVer.Version(testCase.Item1);
+                Assert.Equal(version.Patch, testCase.Item2);
+            }
+        }
+    }
+}
+
--- SemVer/Version.cs
+++ SemVer/Version.cs
@@ -1,11 +1,42 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace SemVer
 {
     public class Version
     {
-        public Version ()
+        public int Major { get; set; }
+
+        public int Minor { get; set; }
+
+        public int Patch { get; set; }
+
+        public Version(string input)
         {
+            string pattern = @"^
+                [v=\s]*
+                (\d+)                    # major version
+                \.
+                (\d+)                    # minor version
+                \.
+                (\d+)                    # patch version
+                (\-([0-9A-Za-z\-\.]+))?  # pre-release version
+                (\+([0-9A-Za-z\-\.]+))?  # build metadata
+                \s*
+                $";
+
+            var regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
+            var match = regex.Match(input);
+            if (!match.Success)
+            {
+                throw new ArgumentException("Invalid version string");
+            }
+
+            Major = Int32.Parse(match.Groups[1].Value);
+
+            Minor = Int32.Parse(match.Groups[2].Value);
+
+            Patch = Int32.Parse(match.Groups[3].Value);
         }
     }
 }
