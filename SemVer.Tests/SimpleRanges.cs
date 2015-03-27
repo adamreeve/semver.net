f15866ecaca47e44a55f20ba26d705110c98ef33
Adam Reeve <adreeve@gmail.com>
2015-03-27 20:48:27 +1300

    Add test for Range.Select

--- SemVer.Tests/SimpleRanges.cs
+++ SemVer.Tests/SimpleRanges.cs
@@ -26,5 +26,22 @@ namespace SemVer.Tests
             Assert.False(range.Match(new Version("1.2.8")));
             Assert.False(range.Match(new Version("2.0.0")));
         }
+
+        [Fact]
+        public void TestSelect()
+        {
+            var versions = new [] {
+                new Version("1.2.7"),
+                new Version("1.2.8"),
+                new Version("1.2.99"),
+                new Version("1.2.6"),
+                new Version("1.3.0"),
+                new Version("1.1.0"),
+            };
+            var range = new Range(">=1.2.7 <1.3.0");
+            Assert.Equal(
+                    new Version("1.2.99"),
+                    range.Select(versions));
+        }
     }
 }
