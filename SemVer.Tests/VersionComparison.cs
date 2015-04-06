7456d1b94c3aa2e3eedfc99325c7dffbb630d685
Adam Reeve <adreeve@gmail.com>
2015-04-06 19:54:45 +1200

    Add version comparison tests from node-semver

--- SemVer.Tests/VersionComparison.cs
+++ SemVer.Tests/VersionComparison.cs
@@ -99,5 +99,104 @@ namespace SemVer.Tests
             var versionB = new Version(b);
             Assert.False(versionA <= versionB);
         }
+
+        // Comparison tests from npm/node-semver
+        [Theory]
+        [InlineData("0.0.0", "0.0.0-foo")]
+        [InlineData("0.0.1", "0.0.0")]
+        [InlineData("1.0.0", "0.9.9")]
+        [InlineData("0.10.0", "0.9.0")]
+        [InlineData("0.99.0", "0.10.0")]
+        [InlineData("2.0.0", "1.2.3")]
+        [InlineData("v0.0.0", "0.0.0-foo")]
+        [InlineData("v0.0.1", "0.0.0")]
+        [InlineData("v1.0.0", "0.9.9")]
+        [InlineData("v0.10.0", "0.9.0")]
+        [InlineData("v0.99.0", "0.10.0")]
+        [InlineData("v2.0.0", "1.2.3")]
+        [InlineData("0.0.0", "v0.0.0-foo")]
+        [InlineData("0.0.1", "v0.0.0")]
+        [InlineData("1.0.0", "v0.9.9")]
+        [InlineData("0.10.0", "v0.9.0")]
+        [InlineData("0.99.0", "v0.10.0")]
+        [InlineData("2.0.0", "v1.2.3")]
+        [InlineData("1.2.3", "1.2.3-asdf")]
+        [InlineData("1.2.3", "1.2.3-4")]
+        [InlineData("1.2.3", "1.2.3-4-foo")]
+        [InlineData("1.2.3-5-foo", "1.2.3-5")]
+        [InlineData("1.2.3-5", "1.2.3-4")]
+        [InlineData("1.2.3-5-foo", "1.2.3-5-Foo")]
+        [InlineData("3.0.0", "2.7.2+asdf")]
+        [InlineData("1.2.3-a.10", "1.2.3-a.5")]
+        [InlineData("1.2.3-a.b", "1.2.3-a.5")]
+        [InlineData("1.2.3-a.b", "1.2.3-a")]
+        [InlineData("1.2.3-a.b.c.10.d.5", "1.2.3-a.b.c.5.d.100")]
+        [InlineData("1.2.3-r2", "1.2.3-r100")]
+        [InlineData("1.2.3-r100", "1.2.3-R2")]
+        public void Comparisons(string v0s, string v1s)
+        {
+            var v0 = new Version(v0s);
+            var v1 = new Version(v1s);
+            Assert.True(v0 > v1);
+            Assert.True(v1 < v0);
+            Assert.False(v1 > v0);
+            Assert.False(v0 < v1);
+            Assert.Equal(v0, v0);
+            Assert.Equal(v1, v1);
+            Assert.NotEqual(v0, v1);
+        }
+
+        // Equality tests from npm/node-semver
+        [Theory]
+        [InlineData("1.2.3", "v1.2.3")]
+        [InlineData("1.2.3", "=1.2.3")]
+        [InlineData("1.2.3", "v 1.2.3")]
+        [InlineData("1.2.3", "= 1.2.3")]
+        [InlineData("1.2.3", " v1.2.3")]
+        [InlineData("1.2.3", " =1.2.3")]
+        [InlineData("1.2.3", " v 1.2.3")]
+        [InlineData("1.2.3", " = 1.2.3")]
+        [InlineData("1.2.3-0", "v1.2.3-0")]
+        [InlineData("1.2.3-0", "=1.2.3-0")]
+        [InlineData("1.2.3-0", "v 1.2.3-0")]
+        [InlineData("1.2.3-0", "= 1.2.3-0")]
+        [InlineData("1.2.3-0", " v1.2.3-0")]
+        [InlineData("1.2.3-0", " =1.2.3-0")]
+        [InlineData("1.2.3-0", " v 1.2.3-0")]
+        [InlineData("1.2.3-0", " = 1.2.3-0")]
+        [InlineData("1.2.3-1", "v1.2.3-1")]
+        [InlineData("1.2.3-1", "=1.2.3-1")]
+        [InlineData("1.2.3-1", "v 1.2.3-1")]
+        [InlineData("1.2.3-1", "= 1.2.3-1")]
+        [InlineData("1.2.3-1", " v1.2.3-1")]
+        [InlineData("1.2.3-1", " =1.2.3-1")]
+        [InlineData("1.2.3-1", " v 1.2.3-1")]
+        [InlineData("1.2.3-1", " = 1.2.3-1")]
+        [InlineData("1.2.3-beta", "v1.2.3-beta")]
+        [InlineData("1.2.3-beta", "=1.2.3-beta")]
+        [InlineData("1.2.3-beta", "v 1.2.3-beta")]
+        [InlineData("1.2.3-beta", "= 1.2.3-beta")]
+        [InlineData("1.2.3-beta", " v1.2.3-beta")]
+        [InlineData("1.2.3-beta", " =1.2.3-beta")]
+        [InlineData("1.2.3-beta", " v 1.2.3-beta")]
+        [InlineData("1.2.3-beta", " = 1.2.3-beta")]
+        [InlineData("1.2.3-beta+build", " = 1.2.3-beta+otherbuild")]
+        [InlineData("1.2.3+build", " = 1.2.3+otherbuild")]
+        [InlineData("1.2.3-beta+build", "1.2.3-beta+otherbuild")]
+        [InlineData("1.2.3+build", "1.2.3+otherbuild")]
+        [InlineData(" v1.2.3+build", "1.2.3+otherbuild")]
+        public void Equality(string v0s, string v1s)
+        {
+            var v0 = new Version(v0s);
+            var v1 = new Version(v1s);
+
+            Assert.True(v0.Equals(v1));
+            Assert.True(v0 == v1);
+            Assert.False(v0 != v1);
+            Assert.False(v0 > v1);
+            Assert.True(v0 >= v1);
+            Assert.False(v0 < v1);
+            Assert.True(v0 <= v1);
+        }
     }
 }
