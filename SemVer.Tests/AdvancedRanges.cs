723af8a8f8b9569550a9750ede39a3b12b4aab4b
Adam Reeve <adreeve@gmail.com>
2015-04-06 22:24:47 +1200

    Remove node-semver tests for undocumented range spec

--- SemVer.Tests/AdvancedRanges.cs
+++ SemVer.Tests/AdvancedRanges.cs
@@ -141,10 +141,7 @@ namespace SemVer.Tests
         [InlineData("2.3", "2.3.1")]
         [InlineData("~2.4", "2.4.0")]  // >=2.4.0 <2.5.0
         [InlineData("~2.4", "2.4.5")]
-        [InlineData("~>3.2.1", "3.2.2")]  // >=3.2.1 <3.3.0,
         [InlineData("~1", "1.2.3")]  // >=1.0.0 <2.0.0
-        [InlineData("~>1", "1.2.3")]
-        [InlineData("~> 1", "1.2.3")]
         [InlineData("~1.0", "1.0.2")]  // >=1.0.0 <1.1.0,
         [InlineData("~ 1.0", "1.0.2")]
         [InlineData("~ 1.0.3", "1.0.12")]
