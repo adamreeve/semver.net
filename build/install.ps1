a121c7ef9b51e89157c3da6b995efbdc61eb44d0
Adam Reeve <adreeve@gmail.com>
2023-10-17 12:49:21 +1300

    Build and project tidy up
    * Remove appveyor config and build scripts
    * Only support netstandard2.0
    * Store signing key unencrypted (as recommended at https://learn.microsoft.com/en-us/dotnet/standard/library-guidance/strong-naming#create-strong-named-net-libraries)

--- build/install.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-Expression ".\build\dotnet-install.ps1 -Channel Current -Version 2.0.0"
---
+++ src/SemanticVersioning/InternalsVisibleTo.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("SemanticVersioning.Tests, PublicKey=" +
+        "002400000480000094000000060200000024000052534131000400000100010019351d" +
+        "4288017757df1b69b4d0da9a775e6eec498ec93d209d6db4d62e9962476c8da01545cc" +
+        "47335cdc39ba803f4db368ce5f2fdd6cd395196f3328f9039dccdeb3c0f9aece7b8751" +
+        "cd3bc2cb2297d4f463a376eff61b7295b96af9b9faf3eef6005dc967a7a97431cc42cf" +
+        "f72e60f05797f3e16186f8fbaf26074e96a2b5e1")]
